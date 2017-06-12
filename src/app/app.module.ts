e8b66a735ed91af89829178632176f6d007a0215
RVerma90 <irahulverma90@gmail.com>
2017-06-13 00:16:40 +0100

    Added Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 // HttpModule required, and must be added to imports for Http request
 import { HttpModule } from '@angular/http';
+// MaterialModule from Material design
+import { MaterialModule } from "@angular/material";
+// BrowserAnimationsModule for animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+// MasonryModule is a layout library
+import { MasonryModule } from 'angular2-masonry';
 import { AppComponent } from './app.component';
 import { ImageListComponent } from './image-list/image-list.component';
 
 import { ImageService } from './shared/image.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ImageListComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [ImageService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ImageListComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        MasonryModule
+    ],
+    providers: [ImageService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
