765fe359e8b86f47817eef0d5b73bb9279f07a34
RVerma90 <irahulverma90@gmail.com>
2017-06-12 22:17:08 +0100

    Successful Request to Pixabay

---
+++ src/app/image-list/image-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../shared/image.service';
+
+@Component({
+  selector: 'app-image-list',
+  templateUrl: './image-list.component.html',
+  styleUrls: ['./image-list.component.css']
+})
+export class ImageListComponent implements OnInit {
+  images: any[];
+
+  constructor(private imageService: ImageService) {}
+
+  searchImages(query: string) {
+    return this.imageService.getImage(query).subscribe(
+      data => console.log(data),
+      error => console.log(error),
+      () => console.log("Request complete!")
+    )
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  PIXABAY_API_KEY: '5618139-a02732ae53af31ab66f05b005',
+  PIXABAY_API_URL: 'https://pixabay.com/api/?keys='
+};
